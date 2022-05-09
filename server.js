716a5450f48286397acb73fbbe09174d6e7d2e32
寺部智英 <terabetomohide@terabutomohidenoMacBook-Pro.local>
2022-05-09 16:32:34 +0900

    add socket

--- server.js
+++ server.js
@@ -1,20 +1,72 @@
-'use strict';
-
 const express = require('express');
-const socketIO = require('socket.io');
+const bodyParser = require('body-parser');
+const http = require('http');
+const cors = require('cors')
+const cacheData = require('memory-cache');
+
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+
+const defaultOrigin = "http://localhost:3000";
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const ORIGIN = process.env.ORIGIN || defaultOrigin;
 
-const io = socketIO(server);
+app.use(cors({ORIGIN}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+app.get('/game/:gameId', (req, res) => {
+  const param = req.query;
+  const key = param.gameId;
+
+  const data = cacheData.get(key);
+  if (data) {
+    res.status(200).json(data);
+  } else {
+    res.status(404);
+    res.end();
+  }
+});
+
+const io = socketIo(server,{
+  cors: {
+    origin: ORIGIN,
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+io.on('connection', (socket) => {
+  app.post('/game/:gameId', (req, res) => {
+    const data = req.body;
+    const params = req.params;
+    const key = params.gameId;
+
+    if (typeof key !== "string") {
+      res.status(400)
+      res.end()
+    }
+
+    cacheData.put(key, data, 1000 * 60 * 60 * 24);
+    io.emit(key, data)
+    res.status(201).json(data);
+    res.end()
+  });
+
+  console.log('user connected');
+  socket.emit('connection', 'user connected')
+});
